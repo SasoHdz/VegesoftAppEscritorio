b895e098d5e972f1fbc3586f6a07b963b462874e
SasoHdz <isaacrdzcmd@gmail.com>
2023-09-29 11:13:17 -0600

    Primera pantalla de vegesofht

---
+++ preload.js
@@ -0,0 +1,12 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const type of ['chrome', 'node', 'electron']) {
+      replaceText(`${type}-version`, process.versions[type])
+    }
+  })
