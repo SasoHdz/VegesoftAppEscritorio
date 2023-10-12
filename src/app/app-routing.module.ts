d3f9a3ea129df1e64eb1fd20f38a2fea3b4d7c64
SasoHdz <isaacrdzcmd@gmail.com>
2023-10-12 11:00:19 -0600

    Ventanas iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ConfigComponent } from './pages/config/config.component';
+import { HistorialComponent } from './pages/historial/historial.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home',component: HomeComponent},
   {path:'config',component: ConfigComponent},
+  {path:'history',component: HistorialComponent},
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigComponent } from './pages/config/config.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HistorialComponent } from './pages/historial/historial.component';
+import { MenuVerticalComponent } from './components/menu-vertical/menu-vertical.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConfigComponent,
-    HomeComponent
+    HomeComponent,
+    HistorialComponent,
+    MenuVerticalComponent
   ],
   imports: [
     BrowserModule,
