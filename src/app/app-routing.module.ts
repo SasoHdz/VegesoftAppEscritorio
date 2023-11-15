255951b139e511d0d760e9900dc3bc402d6ce5bf
SasoHdz <isaacrdzcmd@gmail.com>
2023-11-15 11:44:25 -0600

    Add new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ConfigComponent } from './pages/config/config.component';
 import { HistorialComponent } from './pages/historial/historial.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { IniciarComponent } from './pages/iniciar/iniciar.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'home',component: HomeComponent},
   {path:'config',component: ConfigComponent},
   {path:'history',component: HistorialComponent},
+  {path:'inventario',component: InventarioComponent},
+  {path:'iniciar',component: IniciarComponent},
+
+
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HistorialComponent } from './pages/historial/historial.component';
 import { MenuVerticalComponent } from './components/menu-vertical/menu-vertical.component';
 import { PrductoComponent } from './components/prducto/prducto.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { IniciarComponent } from './pages/iniciar/iniciar.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ItemListComponent } from './components/item-list/item-list.component';
     HistorialComponent,
     MenuVerticalComponent,
     PrductoComponent,
-    ItemListComponent
+    ItemListComponent,
+    IniciarComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
