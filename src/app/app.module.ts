0c7e2048b1889020e7d13846d1fbce1ed4f137c2
SasoHdz <isaacrdzcmd@gmail.com>
2023-11-22 18:26:52 -0600

    Pantalla Inicio 50%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { CampoFormComponent } from './components/campo-form/campo-form.component';
 import { ButtonSuccessComponent } from './components/button-success/button-success.component';
 import { ButtonStopComponent } from './components/button-stop/button-stop.component';
+import { ItemVentaComponent } from './components/item-venta/item-venta.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { ButtonStopComponent } from './components/button-stop/button-stop.compon
     LayoutComponent,
     CampoFormComponent,
     ButtonSuccessComponent,
-    ButtonStopComponent
+    ButtonStopComponent,
+    ItemVentaComponent
   ],
   imports: [
     BrowserModule,
